a723e717c97af917e30a62cc96811060f4a1a11d
Joseph Schilz <joseph@schilz.org>
2018-04-04 18:36:44 -0700

    Tabs to spaces.

--- main.py
+++ main.py
@@ -10,43 +10,43 @@ app.secret_key = b'\xb6x(\xd67\x1f\xa7\x15\x92\xf1VqU\xe9|\xbcqu\xac\xf6\x16\xa8
 
 @app.route('/retrieve')
 def retrieve():
-	code = request.args.get('code', None)
+    code = request.args.get('code', None)
 
-	if code is None:
-		return render_template("retrieve.jinja2") 
-	try:
-		saved_total = SavedTotal.get(SavedTotal.code == code)
-	except SavedTotal.DoesNotExist:
-		return render_template("retrieve.jinja2")
-	
-	session['total'] = saved_total.value
+    if code is None:
+        return render_template("retrieve.jinja2") 
+    try:
+        saved_total = SavedTotal.get(SavedTotal.code == code)
+    except SavedTotal.DoesNotExist:
+        return render_template("retrieve.jinja2")
+    
+    session['total'] = saved_total.value
 
-	return redirect(url_for('add'))
-	
+    return redirect(url_for('add'))
+    
 
 @app.route('/add', methods=['GET', 'POST'])
 def add():
-	if 'total' not in session:
-		session['total'] = 0
-	if request.method == 'POST':
-		# Handle the form submission
-		number = int(request.form['number'])
-		session['total'] += number
+    if 'total' not in session:
+        session['total'] = 0
+    if request.method == 'POST':
+        # Handle the form submission
+        number = int(request.form['number'])
+        session['total'] += number
 
-	return render_template('add.jinja2', session=session)
+    return render_template('add.jinja2', session=session)
 
 @app.route('/save', methods=['POST'])
 def save():
-	total = session.get('total', 0)
-	code = base64.b32encode(os.urandom(8)).decode().strip('=')
+    total = session.get('total', 0)
+    code = base64.b32encode(os.urandom(8)).decode().strip('=')
 
-	saved_total = SavedTotal(value=total, code=code)
-	saved_total.save()
+    saved_total = SavedTotal(value=total, code=code)
+    saved_total.save()
 
-	return render_template('save.jinja2', code=code)
+    return render_template('save.jinja2', code=code)
 
 
 if __name__ == "__main__":
-	port = int(os.environ.get("PORT", 6738))
-	app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 6738))
+    app.run(host='0.0.0.0', port=port)
 
--- model.py
+++ model.py
@@ -6,8 +6,8 @@ from playhouse.db_url import connect
 db = connect(os.environ.get('DATABASE_URL', 'sqlite:///my_database.db'))
 
 class SavedTotal(Model):
-	code = CharField(max_length=255, unique=True)
-	value = IntegerField()
+    code = CharField(max_length=255, unique=True)
+    value = IntegerField()
 
-	class Meta:
-		database = db
+    class Meta:
+        database = db
